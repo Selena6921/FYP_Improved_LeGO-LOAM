cc89a57ac2c4906d439c6c5fd1741ee3623e577e
Selena <116261109+Selena6921@users.noreply.github.com>
2024-06-21 18:26:28 +0100

    Patchwork-Enhanced

---
+++ LeGO-LOAM-Patchwork/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/selena/catkin_ws/src/LeGO-LOAM-Patchwork/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/selena/catkin_ws/src/LeGO-LOAM-Patchwork/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/melodic/bin:/home/selena/.vscode-server/bin/89de5a8d4d6205e5b11647eb6a74844ca23d2573/bin/remote-cli:/opt/cmake/bin:/opt/cmake/bin:/opt/cmake/bin:/opt/cmake/bin:/home/selena/.local/bin:/opt/cmake/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Program Files/WindowsApps/CanonicalGroupLimited.Ubuntu18.04LTS_1804.6.4.0_x64__79rhkp1fndgsc:/mnt/c/Windows/system32:/mnt/c/Windows:/mnt/c/Windows/System32/Wbem:/mnt/c/Windows/System32/WindowsPowerShell/v1.0/:/mnt/c/Windows/System32/OpenSSH/:/mnt/c/Program Files/Bandizip/:/mnt/c/Program Files/MATLAB/R2023b/bin:/mnt/c/Program Files/MATLAB/R2022b/runtime/win64:/mnt/c/Program Files/MATLAB/R2022b/bin:/mnt/c/Program Files/7-Zip:/mnt/c/Users/Selin/AppData/Local/Microsoft/WindowsApps:/mnt/d/Microsoft VS Code/bin:/mnt/d/intelFPGA_lite/18.1/modelsim_ase/win32aloem:/mnt/c/intelFPGA_lite_new/18.1/modelsim_ase/win32aloem:/mnt/c/Program Files (x86)/NVIDIA Corporation/PhysX/Common:/mnt/c/WINDOWS:/mnt/c/Windows/system32/Wbem:/mnt/c/Windows/system32/WindowsPowerShell/v1.0/:/mnt/c/ic-utils:/mnt/c/Windows/system32/OpenSSH/:/mnt/c/Program Files/dotnet/:/mnt/c/Program Files (x86)/Windows Kits/8.1/Windows Performance Toolkit/:/snap/bin'
+export PKG_CONFIG_PATH="/home/selena/catkin_ws/src/LeGO-LOAM-Patchwork/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/selena/catkin_ws/src/LeGO-LOAM-Patchwork/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/selena/catkin_ws/src/LeGO-LOAM-Patchwork/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/selena/catkin_ws/src/LeGO-LOAM-Patchwork/LeGO-LOAM-Patchwork/LeGO-LOAM-Patchwork:$ROS_PACKAGE_PATH"
